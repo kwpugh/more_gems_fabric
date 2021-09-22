88e785d9e5554d1997d66571b28336b41ee46ce9
Kevin Pugh <kwpugh@comcast.net>
2021-09-22 09:46:32 -0500

    fix deepslate/nether ore spawn

--- src/main/java/com/kwpugh/more_gems/mixin/DefaultBiomeFeaturesMixin.java
+++ src/main/java/com/kwpugh/more_gems/mixin/DefaultBiomeFeaturesMixin.java
@@ -27,12 +27,18 @@ public class DefaultBiomeFeaturesMixin
     builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_RUBY_OVERWORLD);
     builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_CORUNDUM_OVERWORLD);
     builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_CARBONADO_OVERWORLD);
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_SAPPHIRE_OVERWORLD_DEEPSLATE);
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_RUBY_OVERWORLD_DEEPSLATE);
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_CARBONADO_OVERWORLD_DEEPSLATE);
   }
 
   // Inject Nether Ore into the Nether
   @Inject(method = "addNetherMineables",  at = @At(value = "TAIL"))
   private static void moregemsAddNetherMineables(Builder builder, CallbackInfo ci)
   {
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_KUNZITE_NETHER);
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_ALEXANDRITE_NETHER);
+    builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_CORUNDUM_NETHER);
     builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_MOISSANITE_NETHER);
     builder.feature(GenerationStep.Feature.UNDERGROUND_ORES, OreGen.ORE_MOISSANITE_NETHER_HIGH);
 
